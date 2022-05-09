c2deb22ddcb0dd6a9d36c47b559695eaca831966
Dean Henze <deanchenze@gmail.com>
2022-05-08 18:00:30 -0700

    Placeholder for script to compute fluxes from cospectra.

---
+++ analyses/WP3_fluxes/fluxes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 17:28:45 2022
+
+@author: Dean
+
+Fluxes for P-3 level legs, computed from cospectra. 
+"""
+
+
+
+    # Fluxes
+        #...
+
+    # Correction for number of imputed data points:
+        #...
+
