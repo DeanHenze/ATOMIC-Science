de3303e80500faaefdaf544f6b25b4db4ccae19c
Dean Henze <deanchenze@gmail.com>
2022-05-12 17:12:24 -0700

    Script to piecewise linearly scale the altitude. E.g. to scale by ML top and trade inversion.

---
+++ analyses/WP3_fluxes/rangescaler.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 13:47:26 2022
+
+@author: Dean
+"""
+
+
+
+import numpy as np
+#from sklearn.preprocessing import minmax_scale
+import matplotlib.pyplot as plt
+
+
+
+def piecewise_linscale(x, x_bnds, xscaled_bnds):
+    """
+    Linear scaling of piecewise segments. Each segment is scaled to specified 
+    bounds.
+    
+    Inputs
+    ------
+    x: iterable of lists / arrays.
+        Each element of x is a piecewise segment. Even if only one piece, 
+        it should be put in an iterable.
+    
+    xscale_bnds: iterable of 2-tuples.
+        Same length as x. The bounds to scale each segment by.
+    """
+    #if len(x) != len(xscale_bnds):
+    #    print("x and xscale_bnds are not the same length")
+    #    return
+    
+    
+    # Break x into piecewise segments:
+    xpiece = []
+    for bnds in x_bnds:
+        b1 = bnds[0]
+        b2 = bnds[1]
+        xpiece.append(x[np.where((x>=b1) & (x<b2))])
+        
+        
+    # Scaled segments:
+    xpiece_scaled = []
+    for xp, bnds, bnds_scaled in zip(xpiece, x_bnds, xscaled_bnds):
+        b1 = bnds[0]; b2 = bnds[1]
+        bs1 = bnds_scaled[0]; bs2 = bnds_scaled[1]
+        x_std = (xp - b1) / (b2 - b1)
+        xpiece_scaled.append(x_std * (bs2 - bs1) + bs1)
+
+        
+    # Reshape:
+    xscaled = np.array([])
+    for xp in xpiece_scaled:
+        xscaled = np.append(xscaled, xp)
+        
+    return xscaled
+    
+
+x = np.arange(0,100,1)
+x_bnds = [(0,20), (20,40), (40,60), (60,100)]
+xscaled_bnds = [(1,2), (2,5), (5,10), (10,11)]
+
+xs = piecewise_linscale(x, x_bnds, xscaled_bnds)
+
+plt.figure()
+plt.scatter(x, xs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
