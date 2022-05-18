8f8e331b58d42e6e4977de8eb6b49bf3ba0e1dfb
Dean Henze <deanchenze@gmail.com>
2022-05-18 12:13:43 -0700

    Load dropsonde filenames.

--- analyses/WP3_cloudmodule_char/lcl_p3cloudmods.py
+++ analyses/WP3_cloudmodule_char/lcl_p3cloudmods.py
@@ -10,3 +10,38 @@ LCL calcs will use near-surface data from the dropsondes released during each
 cloud module.
 """
 
+
+
+# Built in
+import os
+
+# Local code
+import thermo
+
+
+
+path_drpsnd = "../WP3_cloudmodule_char/cldmod_datafiles/" # Path to cloud mod. dropsondes folder.
+fnames = [f for f in os.listdir(path_drpsnd) 
+          if f.startswith("p3cld_dropsondes") and f.endswith(".nc")
+          ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
