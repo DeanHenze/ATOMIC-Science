d04a50ce691cb5cfeb366b0dc26a9370cd667ddd
Dean Henze <deanchenze@gmail.com>
2022-05-19 09:45:57 -0700

    Script now cycles through each cloud module and computes LCL temperature.

--- analyses/WP3_cloudmodule_char/lcl_p3cloudmods.py
+++ analyses/WP3_cloudmodule_char/lcl_p3cloudmods.py
@@ -15,15 +15,34 @@ cloud module.
 # Built in
 import os
 
+# Third party
+import numpy as np
+import pandas as pd
+import xarray as xr
+
 # Local code
 import thermo
 
 
 
-path_drpsnd = "../WP3_cloudmodule_char/cldmod_datafiles/" # Path to cloud mod. dropsondes folder.
+path_drpsnd = "./cldmod_datafiles/" # Path to cloud mod. dropsondes folder.
 fnames = [f for f in os.listdir(path_drpsnd) 
           if f.startswith("p3cld_dropsondes") and f.endswith(".nc")
           ]
+
+keyalts_tab = pd.read_csv("./cldmod_keyaltitudes.csv")
+
+Tlcl = [] # LCL temperatures appended here.
+ncld_list = np.arange(1,17,1) # 16 Cloud modules over the IOP
+for n in ncld_list:
+    
+    f = [f for f in fnames if "_ncld%s"%str(n).zfill(2) in f][0]
+    drpsnds = xr.load_dataset(path_drpsnd + f)
+    
+    drpsnds_ns = drpsnds.sel(alt=slice(30,100)) # near surface portions.
+    sfcmean = drpsnds_ns.mean()[['ta', 'p', 'q']]
+    
+    Tlcl.append(thermo.Tlcl(sfcmean['ta'], sfcmean['p'], sfcmean['q']).values)
 
 
 
