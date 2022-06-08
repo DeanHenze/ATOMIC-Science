45e668faf5a58207b2e829f39b34c73cf6689c48
Dean Henze <deanchenze@gmail.com>
2022-06-08 15:49:49 -0700

    Tick label setter as separate fxn.

--- analyses/GOES16_cloudchar/imageplot_wp3cldmods.py
+++ analyses/GOES16_cloudchar/imageplot_wp3cldmods.py
@@ -33,7 +33,13 @@ path_goescldmoddir = "./goes16_WP3cldmods/"
 path_savedir = "./images/"
 ##_____________________________________________________________________________
 ## I/O paths   
-    
+ 
+
+def set_imageticklabels(ax):
+    """
+    """
+    ax.set_xticklabels(ax.get_xticks().astype(str), fontsize=6)
+    ax.set_yticklabels(ax.get_yticks().astype(str), fontsize=6)    
 
  
 def createimages(path_goescldmoddir, path_savedir):
@@ -47,38 +53,41 @@ def createimages(path_goescldmoddir, path_savedir):
         ncld_str = f[-9:-3]
 
                 
-        fig_ir = plt.figure(figsize=(4, 4))
+        fig_ir = plt.figure(figsize=(1.25, 1.25))
         ax_ir = fig_ir.add_axes([0.2, 0.2, 0.7, 0.7])
         ax_ir.pcolor(
             goesdata['longitude'], goesdata['latitude'], 
             goesdata['temperature_ir'], 
             cmap = 'gist_yarg', vmin=272, vmax=300
             )
+        set_imageticklabels(ax_ir)
         fnameir_save = "G16V04.0.ATOMIC.PX.02K_IRtemp_%s.png" % ncld_str
-        fig_ir.savefig(os.path.join(path_savedir, fnameir_save))        
+        fig_ir.savefig(os.path.join(path_savedir, fnameir_save), bbox_inches='tight')        
         plt.close(fig=fig_ir)
         
         
-        fig_vis = plt.figure(figsize=(4, 4))
+        fig_vis = plt.figure(figsize=(1.25, 1.25))
         ax_vis = fig_vis.add_axes([0.2, 0.2, 0.7, 0.7])
         ax_vis.pcolor(
             goesdata['longitude'], goesdata['latitude'], 
             goesdata['reflectance_vis'], 
             cmap = 'gray', vmin=0, vmax=0.8
             )
+        set_imageticklabels(ax_vis)
         fnamevis_save = "G16V04.0.ATOMIC.PX.02K_vis-reflectance_%s.png" % ncld_str
-        fig_vis.savefig(os.path.join(path_savedir, fnamevis_save))
+        fig_vis.savefig(os.path.join(path_savedir, fnamevis_save), bbox_inches='tight')
         plt.close(fig=fig_vis)
         
         
-        fig_irmask = plt.figure(figsize=(4, 4))
+        fig_irmask = plt.figure(figsize=(1.25, 1.25))
         ax_irmask = fig_irmask.add_axes([0.2, 0.2, 0.7, 0.7])
         ax_irmask.pcolor(
             goesdata['longitude'], goesdata['latitude'], 
             goesdata['ir_mask'], cmap='gist_yarg'
             )
+        set_imageticklabels(ax_irmask)
         fnameirmask_save = "G16V04.0.ATOMIC.PX.02K_IRmask_%s.png" % ncld_str
-        fig_irmask.savefig(os.path.join(path_savedir, fnameirmask_save))   
+        fig_irmask.savefig(os.path.join(path_savedir, fnameirmask_save), bbox_inches='tight')   
         plt.close(fig=fig_irmask)
     
 
