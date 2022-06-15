24324368a44645a26827abd68693c7a9fbe29fcf
Dean Henze <deanchenze@gmail.com>
2022-06-15 16:37:11 -0700

    Include cubic interpolation before plotting.

--- analyses/WP3_fluxes/profileplotter.py
+++ analyses/WP3_fluxes/profileplotter.py
@@ -18,6 +18,7 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt
+from scipy import interpolate
 
 # Local code
 import thermo
@@ -77,26 +78,49 @@ def plotprf_singlevar(data, ax, altbinwidth=0.33, npts_thresh=2, pcolor='grey'):
     alt_bincenter = np.array(alt_bincenter)
 
 
-    # Remove and levels with less than 2 data points:            
-    lessthan2points = (fluxvar_grouped.count().sum(axis=1) < npts_thresh).values
-    alt_bincenter = alt_bincenter[~lessthan2points]
-    meanprf = meanprf[~lessthan2points]
-    medianprf = medianprf[~lessthan2points]
-    minvals = minvals[~lessthan2points]
-    maxvals = maxvals[~lessthan2points]
+    # Remove and levels with less than data point threshold:            
+    ltnpoints = (fluxvar_grouped.count().sum(axis=1) < npts_thresh).values
+    alt_bincenter = alt_bincenter[~ltnpoints]
+    meanprf = meanprf[~ltnpoints]
+    medianprf = medianprf[~ltnpoints]
+    minvals = minvals[~ltnpoints]
+    maxvals = maxvals[~ltnpoints]
+    
+    
+    # Spline interpolation:
+    alts_interp = np.arange(min(alt_bincenter), max(alt_bincenter), altbinwidth/1.5)
+    alts_interp = np.append(alts_interp, max(alt_bincenter))
+    meanprf_interp = interpolate.interp1d(alt_bincenter, meanprf, kind='cubic')(alts_interp)
+    medianprf_interp = interpolate.interp1d(alt_bincenter, medianprf, kind='cubic')(alts_interp)
+    minvals_interp = interpolate.interp1d(alt_bincenter, minvals, kind='cubic')(alts_interp)
+    maxvals_interp = interpolate.interp1d(alt_bincenter, maxvals, kind='cubic')(alts_interp)
 
 
     # Plot:
+    #ax.fill_betweenx(
+    #    alt_bincenter, minvals, x2=maxvals, 
+    #    color=pcolor, alpha=0.3, 
+    #    )
+    #ax.plot(
+    #    meanprf, alt_bincenter, 
+    #    color=pcolor, linestyle='-', linewidth=3, zorder=10
+    #    )
+    #ax.plot(
+    #    medianprf, alt_bincenter, 
+    #    color=pcolor, linestyle='--', linewidth=2, zorder=10
+    #    )
+    
     ax.fill_betweenx(
-        alt_bincenter, minvals, x2=maxvals, 
+        alts_interp, minvals_interp, x2=maxvals_interp, 
         color=pcolor, alpha=0.3, 
-        #edgecolor='none'
         )
     ax.plot(
-        meanprf, alt_bincenter, 
+        meanprf_interp, alts_interp, 
         color=pcolor, linestyle='-', linewidth=3, zorder=10
         )
     ax.plot(
-        medianprf, alt_bincenter, 
+        medianprf_interp, alts_interp, 
         color=pcolor, linestyle='--', linewidth=2, zorder=10
         )
+ 
+    
